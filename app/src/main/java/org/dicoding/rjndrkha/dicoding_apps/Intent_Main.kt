eada453795333d44e4bfdc854a5c59a83515a5e4
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2023-02-27 16:02:33 +0700

    Activity & Intent DONE

---
+++ app/src/main/java/org/dicoding/rjndrkha/dicoding_apps/Intent_Main.kt
@@ -0,0 +1,16 @@
+package org.dicoding.rjndrkha.dicoding_apps
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Intent_Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_page)
+
+    }
+
+
+
+}
